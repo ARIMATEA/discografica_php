028e33593b1cf8dae624f7de44b1d04dcf070434
Ricardo <ricardo.b.silva13@gmail.com>
2016-03-29 21:21:52 -0300

    cadastro de musicas implementado

--- controller/MusicasController.php
+++ controller/MusicasController.php
@@ -7,5 +7,17 @@ class MusicasController {
         return $musica->listarTodasAsMusicas();
     }
     
+    public function inserir(){
+        $musica = new Musica();
+        $titulo = $_POST['titulo'];
+        $autor_id = $_POST['autor_id'];
+        $midia_id = $_POST['midia_id'];
+        
+        $musica->setTitulo($titulo);
+        $musica->setAutor_id($autor_id);
+        $musica->setMidia_id($midia_id);
+        $musica->salvar();
+    }
+    
 }
 
--- model/Musica.php
+++ model/Musica.php
@@ -1,9 +1,57 @@
 <?php
 include '../conexao/Conexao.php';
 class Musica {
-    public $nome;
-    public $autor_id;
+    private $id;
+    private $titulo;
+    private $autor_id;
+    private $midia_id;
     
+    function getId() {
+        return $this->id;
+    }
+
+    function getTitulo() {
+        return $this->titulo;
+    }
+
+    function getAutor_id() {
+        return $this->autor_id;
+    }
+
+    function getMidia_id() {
+        return $this->midia_id;
+    }
+
+    function setId($id) {
+        $this->id = $id;
+    }
+
+    function setTitulo($titulo) {
+        $this->titulo = $titulo;
+    }
+
+    function setAutor_id($autor_id) {
+        $this->autor_id = $autor_id;
+    }
+
+    function setMidia_id($midia_id) {
+        $this->midia_id = $midia_id;
+    }
+
+    public function salvar(){
+        $sql = "INSERT INTO "
+                . "musicas "
+                . "(titulo,autor_id,midia_id) "
+                . "values"
+                . " (:titulo , :autor_id , :midia_id)";
+        $query = Conexao::prepare($sql);
+        $query->bindValue(":titulo", $this->getTitulo());
+        $query->bindValue(":autor_id", $this->getAutor_id());
+        $query->bindValue(":midia_id",  $this->getMidia_id());
+        $query->execute();
+    }    
+   
+        
     public function listarTodasAsMusicas() {
         $sql = "SELECT * FROM musicas";
         $query = Conexao::prepare($sql);
